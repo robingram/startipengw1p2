d68803261a916160ce9e99132befdb4fe0879fab
Rob Ingram <rob@headcrash.org>
2013-06-23 11:03:46 +0100

    Solution to HW2

---
+++ hw2.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+var fs = require('fs');
+var outfile = "primes.txt";
+var out = '';
+
+for (var i = 2; i <= 100; i++) {
+	if (isPrime(i)) {
+		if (out != '') {
+			out += ',';
+		}
+		out += i;
+	}
+}
+
+fs.writeFileSync(outfile, out);  
+console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+
+function isPrime(number) {
+	for (var i = number - 1; i > 1; i--) {
+		var factor = number / parseFloat(i);
+		if (factor % 1 === 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
